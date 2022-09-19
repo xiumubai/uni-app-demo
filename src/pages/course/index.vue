76d215849b4aaa3aa4569239a50f503df1a3eb99
xiumubai <1547702880@qq.com>
2022-09-19 14:08:10 +0800

    feat: 添加uni-ui

--- src/pages/course/index.vue
+++ src/pages/course/index.vue
@@ -1,40 +1,15 @@
 <template>
-  <view class="content"> 课程页 </view>
+  <view class="content"> 课程 </view>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: "Hello",
-    };
-  },
-  onLoad() {},
-  methods: {},
+  name: "course",
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin: 200rpx auto 50rpx auto;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  background: red;
 }
 </style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -4,37 +4,12 @@
 
 <script>
 export default {
-  data() {
-    return {
-      title: "Hello",
-    };
-  },
-  onLoad() {},
-  methods: {},
+  name: "my",
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin: 200rpx auto 50rpx auto;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  background: red;
 }
 </style>
